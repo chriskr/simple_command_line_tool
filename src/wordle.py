d23938a0c6f0a2649ab23f5dcd97defa65f16b1b
Christian Krebs <christian.krebs.pi@gmail.com>
2022-02-13 13:54:02 +0100

    Added more options

--- src/wordle.py
+++ src/wordle.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import string
 
 DICT = 'dict.txt'
 FIVE_LETTERS_WORDS = 'five-letter-words.txt'
@@ -12,34 +13,107 @@ def toFiveLetterWords():
             target.write(''.join([line for line in src if len(line) == 6]))
 
 
-def containsChars(word, chars):
+def contains(word, chars):
     return all([c in word for c in chars])
 
 
-def startswithChars(word, chars):
+def notcontains(word, chars):
+    return all([not c in word for c in chars])
+
+
+def startswith(word, chars):
     return word.startswith(chars)
 
 
-def endswithChars(word, chars):
+def pos2(word, chars):
+    return word[1] == chars
+
+
+def pos3(word, chars):
+    return word[2] == chars
+
+
+def pos4(word, chars):
+    return word[3] == chars
+
+
+def endswith(word, chars):
     return word.endswith(chars)
+
+
+def weight(word, histo):
+    return sum([histo[c] for c in word])
+
+
+def getHistogram():
+    histo = dict([(c, 0) for c in string.ascii_lowercase])
+    with open(os.path.join(SOURCE_ROOT, FIVE_LETTERS_WORDS)) as f:
+        for word in f.readlines():
+            for i in range(5):
+                histo[word[i]] += 1
+    return histo
+
+
+def toUniqueCharWords(list):
+    return [word for word in list if len(set(word.strip())) == 5]
+
+
+def sortByWeight(list, histo):
+    return [word for (word, _) in sorted([(word, weight(word.strip(), histo))
+                                          for word in list], key=lambda t: t[1], reverse=True)]
 
 
 def main_func():
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--contains",
                         help="Search 5 letter words containing given letters")
+    parser.add_argument("-n", "--notcontains",
+                        help="Search 5 letter words not containing given letters")
     parser.add_argument(
         "-s", "--startswith", help="Search 5 letter words starting with given letters")
     parser.add_argument("-e", "--endswith",
                         help="Search 5 letter words ending with given letters")
+    parser.add_argument("-2", "--pos2",
+                        help="Search 5 letter words with given letter at second position")
+    parser.add_argument("-3", "--pos3",
+                        help="Search 5 letter words with given letter at third position")
+    parser.add_argument("-4", "--pos4",
+                        help="Search 5 letter words with given letter at forth position")
+    parser.add_argument('-u', '--onlyUnique', action='store_true',
+                        help='Only include words with unique letters')
+    parser.add_argument('-w', '--sortByWeight', action='store_true',
+                        help='Sort results by frequency of letters')
+    parser.add_argument('-m', '--max', type=int,
+                        help='Return only MAX results')
+
     args = parser.parse_args()
-    matchers = [matcher for matcher in [(startswithChars, args.startswith), (
-        containsChars, args.contains), (endswithChars, args.endswith)] if matcher[1]]
+
+    matchers = [matcher for matcher in [
+        (startswith, args.startswith),
+        (endswith, args.endswith),
+        (contains, args.contains),
+        (notcontains, args.notcontains),
+        (pos2, args.pos2),
+        (pos3, args.pos3),
+        (pos4, args.pos4)
+    ] if matcher[1]]
+
     with open(os.path.join(SOURCE_ROOT, FIVE_LETTERS_WORDS)) as f:
         matches = [word for word in f.readlines() if all(
             [matcher(word.strip('\n'), chars) for (matcher, chars) in matchers])]
+
+        if (args.onlyUnique):
+            matches = toUniqueCharWords(matches)
+
+        if (args.sortByWeight):
+            matches = sortByWeight(matches, getHistogram())
+
+        if (args.max):
+            matches = matches[0:args.max]
+
         print(''.join(matches))
 
 
 if __name__ == "__main__":
+    # charHisto()
     main_func()
